515f7360e4486e02a11127843248553d6439bddc
manikbhandari <mbbhandarimanik2@gmail.com>
2019-02-01 16:20:56 +0530

    added

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,7 @@
 #convert raw corpus, core dict and full dict to feedable form
 from utils import *
 from collections import Counter
+from nltk import ngrams
 import os
 
 def read_corpus(raw_fname, one_wrd=False):
@@ -23,6 +24,28 @@ def read_corpus(raw_fname, one_wrd=False):
 
     assert(len(line) == 0)                  #ensure file ends in a blank line
     return joined_corpus
+
+def read_core_dict(core_dict_fname):
+    """
+    :core_dict_fname: name of domain specific dictionary
+    :returns: dict of label: entity
+    """
+    lines = read(core_dict_fname)
+    core_dict = {}
+    for line in lines:
+        core_dict[line.split('\t')[1]] = line.split('\t')[0]
+
+    return core_dict
+
+def read_full_dict(full_dict_fname):
+    """
+    :full_dict_fname: name of full dictionary (this includes core dictionary provided by AutoNER)
+    :returns: dict of entity: 1
+    """
+    lines = read(full_dict_fname)
+    lines = {wrd: 1 for wrd in lines}
+    return lines
+
 
 def create_vocab(corpus, dataset):
     """
@@ -67,8 +90,8 @@ def ck_to_txt(ck_fname, dataset, split='dev'):
 
     assert(len(tmp_text) == 0)                  #ensure file ends in a blank line
     write(text, '../data/{}/{}/in.txt'.format(dataset, split))
-    write(tb, '../data/{}/{}/in_tb.txt'.format(dataset, split))
-    write(lbl, '../data/{}/{}/in_lbl.txt'.format(dataset, split))
+    write(tb,   '../data/{}/{}/out_tb.txt'.format(dataset, split))
+    write(lbl,  '../data/{}/{}/out_lbl.txt'.format(dataset, split))
     
 def create_train_data(corpus, core_dict, full_dict, dataset):
     """
@@ -77,12 +100,50 @@ def create_train_data(corpus, core_dict, full_dict, dataset):
     :full_dict: each line is entity
     :returns: list of shape of corpus. 1 denotes break b'w w_i and w_{i-1}, 0 denotes tie, -1 denotes unk
     """
-    pass
+    N = max([len(ent.split()) for ent in core_dict])
+    N = max([len(ent.split()) for ent in full_dict] + [N])
+
+    tie_or_break = []
+    labels       = []
+    for i, line in enumerate(corpus):
+        n     = max(N, len(line.split()))
+        grams = []
+        tb    = ['I' for _ in range(len(line.split()))]
+        lbl   = ['None' for _ in range(len(line.split()))]
+
+        while n > 0:
+            grams = list(ngrams(line.split(), n))
+
+            for pos, gram in enumerate(grams):                          #pos is the word number in line of the first word of this ngram
+                gram = ' '.join(list(gram))
+
+                if gram in core_dict:
+                    if tb[pos] == 'I' and tb[pos + n - 1] == 'I':           #to avoid consecutive ngrams to be brought together.
+                        tb[pos + 1: pos + n] = ['O']*(n-1)
+
+                    lbl[pos: pos + n] = [core_dict[gram]]*n
+
+                elif gram in full_dict:
+                    if tb[pos] == 'I' and tb[pos + n - 1] == 'I':
+                        tb[pos + 1: pos + n] = ['O']*(n-1)                #not adding labels here since they are None
+            n -= 1
+
+        tie_or_break.append(' '.join(tb))
+        labels.append(' '.join(lbl))
+        if i % 1000 == 0: print("finished line {}".format(i))
+
+    write(corpus,       '../data/{}/train/in.txt'.format(dataset))
+    write(tie_or_break, '../data/{}/train/out_tb.txt'.format(dataset))
+    write(labels,       '../data/{}/train/out_lbl.txt'.format(dataset))
     
 
 if __name__ == "__main__":
-    dataset = 'BC5CDR' 
-    corpus  = read_corpus('../data/{}/raw_corpus.txt'.format(dataset), one_wrd=True)
-    create_vocab(corpus, dataset)
-    ck_to_txt('../data/BC5CDR/truth_dev.ck', dataset, split='dev')
-    ck_to_txt('../data/BC5CDR/truth_test.ck', dataset, split='test')
+    dataset    = 'BC5CDR' 
+    corpus     = read_corpus('../data/{}/raw_corpus.txt'.format(dataset), one_wrd=True)
+    core_dict  = read_core_dict('../data/{}/dict_core.txt'.format(dataset))
+    full_dict  = read_full_dict('../data/{}/dict_full.txt'.format(dataset))
+
+    # create_vocab(corpus, dataset)
+    # ck_to_txt('../data/BC5CDR/truth_dev.ck', dataset, split='dev')
+    # ck_to_txt('../data/BC5CDR/truth_test.ck', dataset, split='test')
+    create_train_data(corpus, core_dict, full_dict, dataset)
