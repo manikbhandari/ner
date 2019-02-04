3d0302ac31b2789f2c337a3d72b0e4f85aa394e3
manikbhandari <mbbhandarimanik2@gmail.com>
2019-02-05 00:51:30 +0530

    update

--- src/make_batches.py
+++ src/make_batches.py
@@ -15,13 +15,39 @@ def make_batches():
     data = sorted(data, key=lambda x: len(x[0].split()))
     
     
+def dump_vocab():
+    """
+    :returns: Dumps wvoc2id, id2wvoc, cvoc2id, id2cvoc, lbl2id, id2lbl
+              Always: I = 1 = break and O = 0 = tie
+    """    
+    wvocab = read('../data/{}/vocab.txt'.format(args.dataset))
+    wvocab = [' '] + wvocab                                                                   #add space as a word to vocab
+
+    wvoc2id = {i: wrd for i, wrd in enumerate(wvocab)}
+    id2wvoc = {wrd: i for i, wrd in enumerate(wvocab)}
+
+    chars = [c for wrd in wvocab for c in wrd]
+    chars = list(set(chars))
+
+    cvoc2id = {i: c for i, c in enumerate(chars)}
+    id2cvoc = {c: i for i, c in enumerate(chars)}
+
+    labels = read('../data/{}/train/out_lbl.txt'.format(args.dataset))
+    labels = [lbl for line in labels for lbl in line.split()]
+    labels = set(labels)
+
+    pdb.set_trace()
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='NER')
 
     parser.add_argument('-dataset', dest="dataset", default='BC5CDR',               help='')
     parser.add_argument('-split',   dest="split",   default='train',                help='train/dev/test')
-    parser.add_argument('-batch',   dest="batch",   default=64,         type=True,  help='train/dev/test')
+    parser.add_argument('-batch',   dest="batch",   default=64,         type=int,  help='train/dev/test')
 
     args = parser.parse_args()
+    dump_vocab()
     make_batches()
+    
